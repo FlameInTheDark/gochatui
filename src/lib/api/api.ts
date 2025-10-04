d1a5b263d7f169adc5438bebfd042a6ef956ca1f
FlameInTheDark <viktorfreedom@gmail.com>
2025-10-04 10:19:05 +0500

    feat: added selected server, selected channel and scroll position to user settings

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -973,6 +973,12 @@ export interface ModelGuildChannelReadState {
      * @memberof ModelGuildChannelReadState
      */
     'last_read_message_id'?: number;
+    /**
+     * 
+     * @type {number}
+     * @memberof ModelGuildChannelReadState
+     */
+    'scroll_position'?: number;
 }
 /**
  * 
@@ -1035,6 +1041,12 @@ export interface ModelUserSettingsData {
      * @memberof ModelUserSettingsData
      */
     'language'?: string;
+    /**
+     * 
+     * @type {number}
+     * @memberof ModelUserSettingsData
+     */
+    'selected_guild'?: number;
 }
 /**
  * 
@@ -1097,6 +1109,12 @@ export interface ModelUserSettingsGuilds {
      * @memberof ModelUserSettingsGuilds
      */
     'read_states'?: Array<ModelGuildChannelReadState>;
+    /**
+     * 
+     * @type {number}
+     * @memberof ModelUserSettingsGuilds
+     */
+    'selected_channel'?: number;
 }
 /**
  * 
